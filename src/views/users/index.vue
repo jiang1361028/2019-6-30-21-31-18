18e85d4f6e50203287506e4e4257c69757aa1aac
jnp <706685409@qq.com>
2019-07-04 09:14:24 +0800

    用户页开始

--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -1,11 +1,28 @@
 <template>
-    <div class="users">用户列表</div>
+  <div class="users">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜素区 -->
+    <div style="margin: 15px 0;">
+      <el-input
+        v-model="query"
+        placeholder="请输入内容"
+        class="input-with-select"
+        style="width:300px;margin-right:15px"
+        @keyup.enter.native="search"
+      >
+        <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
+      </el-input>
+      <el-button type="success" @click="addDialogFormVisible = true">添加用户</el-button>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-
-}
+export default {}
 </script>
 <style lang="less" scoped>
-
 </style>
